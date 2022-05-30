49663858ceea36dcd9199d72604152f97037a554
antony0016 <feather881030@gmail.com>
2022-05-30 14:39:33 +0800

    LoginManagerStore getters loggedIn update.

--- src/store/LoginManagerStore.ts
+++ src/store/LoginManagerStore.ts
@@ -15,29 +15,18 @@ const useLoginManagerStore = defineStore('LoginManagerStore', {
       access: useStorage('access', ''),
       refresh: useStorage('refresh', ''),
     },
-    address:{
+    address: {
       get_token: 'http://localhost:8000/token/',
       refresh_token: 'http://localhost:8000/token/refresh/',
     },
   }),
   getters: {
     loggedIn: function (state) {
-      if (this.token.access != ''){
-        // use useViewControllerStore's data
-        const viewControllerStore = useViewControllerStore();
-        // set navbar item show
-        viewControllerStore.menuSwitch.isShow = true
-          for (var val of viewControllerStore.topBarItems){
-            val['isShow'] = true 
-          }
-      }else if (this.token.access == ''){
-        // use useViewControllerStore's data
-        const viewControllerStore = useViewControllerStore();
-        // set navbar item show
-        viewControllerStore.menuSwitch.isShow = false
-          for (var val of viewControllerStore.topBarItems){
-            val['isShow'] = false 
-          }
+      // use useViewControllerStore's data
+      const viewControllerStore = useViewControllerStore();
+      viewControllerStore.menuSwitch.isShow = true
+      for (var val of viewControllerStore.topBarItems) {
+        val['isShow'] = this.token.access != ''
       }
       return state.token.access.length > 0;
       // return true;
@@ -58,7 +47,7 @@ const useLoginManagerStore = defineStore('LoginManagerStore', {
         })
         .catch(error => {
           console.log(error)
-        })  
+        })
     },
     logout: function () {
       this.token.access = ''
@@ -70,17 +59,17 @@ const useLoginManagerStore = defineStore('LoginManagerStore', {
       router.push('/login')
     },
     refreshtoken: function () {
-      axios.post(this.address.refresh_token, {refresh: this.token.refresh})
-      .then(response => {
-        //console.log(response.data.access)
-        this.token.access = response.data.access
-        axios.defaults.headers.common["Authorization"] = "Bearer " + this.token.access
-        sessionStorage.setItem('access', response.data.access)
-        //console.log(this.token.access)
-      })
-      .catch(error => {
-        console.log(error)
-      })
+      axios.post(this.address.refresh_token, { refresh: this.token.refresh })
+        .then(response => {
+          //console.log(response.data.access)
+          this.token.access = response.data.access
+          axios.defaults.headers.common["Authorization"] = "Bearer " + this.token.access
+          sessionStorage.setItem('access', response.data.access)
+          //console.log(this.token.access)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   }
 })
